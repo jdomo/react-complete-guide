77a39dc71099985c83c51d5864197e7f0c8343bb
Joe Domovitch <jdomovitch@gmail.com>
2019-12-04 11:15:22 -0800

    re-factor conditional content using pre-return constant variable

--- src/App.js
+++ src/App.js
@@ -59,6 +59,24 @@ class App extends React.Component {
       padding: '8px'
     }
 
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> Hobbies: Hoopin </Person>
+          <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, 'Norman!!')}
+          changed={this.nameChangeHandler}
+          />
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+          <Person name={this.state.persons[4].name} age={this.state.persons[4].age}/>
+        </div> 
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
@@ -67,24 +85,7 @@ class App extends React.Component {
         onClick={this.togglePersonsHandler}
         style={style}
         >Toggle people!</button>
-
-        {
-          this.state.showPersons ? 
-            <div>
-              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> Hobbies: Hoopin </Person>
-              <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age} 
-              click={this.switchNameHandler.bind(this, 'Norman!!')}
-              changed={this.nameChangeHandler}
-              />
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-              <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
-              <Person name={this.state.persons[4].name} age={this.state.persons[4].age}/>
-            </div> 
-          : null 
-        }
-
+        {persons}
       </div>
       );
     //return React.createElement('div', {className:'App'}, [React.createElement('h1', null, 'hello world'), React.createElement('p', null, 'it me')])
